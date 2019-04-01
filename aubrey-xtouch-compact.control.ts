997ae3b34aed415f3476ab98d53a53bc4a976381
aubford <musicaubrey@gmail.com>
2019-04-01 01:29:26 -0600

    add send midi to controller logic

--- aubrey-xtouch-compact.control.ts
+++ aubrey-xtouch-compact.control.ts
@@ -3,7 +3,8 @@ loadAPI(8)
 // This is useful during development.
 host.setShouldFailOnDeprecatedUse(true)
 
-host.defineController('Behringer',
+host.defineController(
+  'Behringer',
   'aubrey-xtouch-compact',
   '0.1',
   '07e1d9d2-246b-46a9-b5b5-07cfbf519fa7',
@@ -12,9 +13,12 @@ host.defineController('Behringer',
 host.defineMidiPorts(1, 1)
 host.addDeviceNameBasedDiscoveryPair(['X-TOUCH COMPACT'], ['X-TOUCH COMPACT'])
 
-const NOTE = 0x90
-const OFF = 0x80
-const CC = 0xB0
+enum MidiStatus {
+  NOTE = 0x90,
+  OFF = 0x80,
+  CC = 0xB0
+}
+
 const TRACK_ENCODERS = 'TRACK_ENCODERS'
 const TRACK_ENCODERS_PUSH = 'TRACK_ENCODERS_PUSH'
 const SIDE_ENCODERS = 'SIDE_ENCODERS'
@@ -29,49 +33,124 @@ const BUTTONS_TRANSPORT = 'BUTTONS_TRANSPORT'
 const FOOT_SW_LED = 'FOOT_SW_LED'
 const FOOT_EXP_LED = 'FOOT_EXP_LED'
 
+type Range = [number, number]
+interface RxInterface {
+  type: string,
+  range: Range,
+  midi: MidiStatus,
+  actions?: { [x: string]: number }
+}
+interface MainInterface {
+  rangeA: [number, number],
+  rangeB: [number, number],
+  type: string,
+  midi: MidiStatus
+}
+
+const inRange = (val, range) => range[0] <= val <= range[1]
+const encoderActionInRange = val => inRange(val, [0,12])
+
 class XTouchCompact {
-  mainInterface
-  rxInterface
-  constructor() {
+  mainInterface: MainInterface[]
+  rxInterface: RxInterface[]
+  midiIn: API.MidiIn
+  midiOut: API.MidiOut
+
+  constructor(midiIn, midiOut) {
+    this.midiIn = midiIn
+    this.midiOut = midiOut
+
     const buttonActions = { off: 0, on: 1, blink: 2 }
-    const encoderActions = { off: 0, on: [1,13], blink: [14,26], allOn: 27, allBlinking: 28 }
+    const encoderActions = { off: 0, on: 1 /* offset */, blink: 14 /* offset */, allOn: 27, allBlinking: 28 }
     const offOn = { off: 0, on: 127 }
     const encoderLEDModeActions = { single: 0, pan: 1, fan: 2, spread: 3, trim: 4 }
+    const { NOTE, CC } = MidiStatus
     this.rxInterface = [
-      { name: FADER, range: [1,9], type: CC },
-      { name: TRACK_ENCODERS, range: [26,33], type: CC, actions: encoderActions },
-      { name: TRACK_ENCODERS_PUSH, range: [18,25], type: CC, encoderLEDModeActions },
-      { name: SIDE_ENCODERS, range: [34,41], type: CC, actions: encoderActions },
-      { name: SIDE_ENCODERS_PUSH, range: [18,25], type: CC, encoderLEDModeActions },
-      { name: BUTTONSA, range: [0, 7], type: NOTE, actions: buttonActions },
-      { name: BUTTONSB, range: [8, 15], type: NOTE, actions: buttonActions },
-      { name: BUTTONSC, range: [16, 23], type: NOTE, actions: buttonActions },
-      { name: BUTTONS_FADER, range: [24, 32], type: NOTE, actions: buttonActions },
-      { name: BUTTONS_TRANSPORT, range: [33, 38], type: NOTE, actions: buttonActions },
-      { name: FOOT_SW_LED, range: [42, 42], type: CC, actions: offOn },
-      { name: FOOT_EXP_LED, range: [43, 43], type: CC, actions: offOn },
+      { range: [1, 9], type: FADER, midi: CC },
+      { range: [10, 17], type: TRACK_ENCODERS_PUSH, midi: CC, actions: encoderLEDModeActions },
+      { range: [18, 25], type: SIDE_ENCODERS_PUSH, midi: CC, actions: encoderLEDModeActions },
+      { range: [26, 33], type: TRACK_ENCODERS, midi: CC, actions: encoderActions },
+      { range: [34, 41], type: SIDE_ENCODERS, midi: CC, actions: encoderActions },
+      { range: [42, 42], type: FOOT_SW_LED, midi: CC, actions: offOn },
+      { range: [43, 43], type: FOOT_EXP_LED, midi: CC, actions: offOn },
+      { range: [0, 7], type: BUTTONSA, midi: NOTE, actions: buttonActions },
+      { range: [8, 15], type: BUTTONSB, midi: NOTE, actions: buttonActions },
+      { range: [16, 23], type: BUTTONSC, midi: NOTE, actions: buttonActions },
+      { range: [24, 32], type: BUTTONS_FADER, midi: NOTE, actions: buttonActions },
+      { range: [33, 38], type: BUTTONS_TRANSPORT, midi: NOTE, actions: buttonActions }
     ]
     this.mainInterface = [
-      { name: TRACK_ENCODERS, rangeA: [10, 17], rangeB: [37, 44], type: CC },
-      { name: TRACK_ENCODERS_PUSH, rangeA: [0, 7], rangeB: [55, 62], type: NOTE },
-      { name: SIDE_ENCODERS, rangeA: [18, 25], rangeB: [45, 52], type: CC },
-      { name: SIDE_ENCODERS_PUSH, rangeA: [8, 15], rangeB: [63, 70], type: NOTE },
-      { name: FADER, rangeA: [1, 9], rangeB: [28, 36], type: CC },
-      { name: FADER_TOUCH, rangeA: [101, 109], rangeB: [111, 119], type: CC },
-      { name: BUTTONS_FADER, rangeA: [40, 48], rangeB: [95, 103], type: NOTE },
-      { name: BUTTONSA, rangeA: [16, 23], rangeB: [71, 78], type: NOTE },
-      { name: BUTTONSB, rangeA: [24, 31], rangeB: [79, 86], type: NOTE },
-      { name: BUTTONSC, rangeA: [32, 39], rangeB: [87, 94], type: NOTE },
-      { name: BUTTONS_TRANSPORT, rangeA: [49, 54], rangeB: [104, 109], type: NOTE }
+      {
+        rangeA: [1, 9],
+        rangeB: [28, 36], type: FADER, midi: CC
+      },
+      {
+        rangeA: [10, 17],
+        rangeB: [37, 44], type: TRACK_ENCODERS, midi: CC
+      },
+      {
+        rangeA: [18, 25],
+        rangeB: [45, 52], type: SIDE_ENCODERS, midi: CC
+      },
+      {
+        rangeA: [101, 109],
+        rangeB: [111, 119], type: FADER_TOUCH, midi: CC
+      },
+      {
+        rangeA: [0, 7],
+        rangeB: [55, 62], type: TRACK_ENCODERS_PUSH, midi: NOTE
+      },
+      {
+        rangeA: [8, 15],
+        rangeB: [63, 70], type: SIDE_ENCODERS_PUSH, midi: NOTE
+      },
+      {
+        rangeA: [16, 23],
+        rangeB: [71, 78], type: BUTTONSA, midi: NOTE
+      },
+      {
+        rangeA: [24, 31],
+        rangeB: [79, 86], type: BUTTONSB, midi: NOTE
+      },
+      {
+        rangeA: [32, 39],
+        rangeB: [87, 94], type: BUTTONSC, midi: NOTE
+      },
+      {
+        rangeA: [40, 48],
+        rangeB: [95, 103], type: BUTTONS_FADER, midi: NOTE
+      },
+      {
+        rangeA: [49, 54],
+        rangeB: [104, 109], type: BUTTONS_TRANSPORT, midi: NOTE
+      }
     ]
   }
+
+  actionValueRange = [0,12]
+  sendRx = (type: string, index: number, actionName: string, actionValue?: number) => {
+    const { midi, range, actions } = this.rxInterface.find(rx => rx.type === type)
+    const outIndex = index + range[0]
+    if (!inRange(outIndex, range)) {
+      throw new Error('bad index value for sendRx')
+    }
+    if (actionValue && !inRange(actionValue, this.actionValueRange)) {
+      throw new Error('bad actionValue for sendRx')
+    }
+    const action: number = actions[actionName]
+    this.midiOut.sendMidi(midi, outIndex, actionValue ? action + actionValue : action)
+  }
+
+  getType = (status, data1) => this.mainInterface.find(({ rangeA, rangeB, midi }) =>
+    status === midi && (inRange(data1, rangeA) || inRange(data1, rangeB))).type
 
 }
 
 
 let transport
-
+let hardware
 function init() {
+  hardware = new XTouchCompact(host.getMidiInPort(0), host.getMidiOutPort(0))
   transport = host.createTransport()
   host.getMidiInPort(0).setMidiCallback(onMidi)
 
